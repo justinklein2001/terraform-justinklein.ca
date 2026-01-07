dff992757f70872bd2d2bfae027fe4f8f61cb200
Justin Klein <justino2001@hotmail.ca>
2026-01-07 17:42:43 -0500

    add local github oidc arn

--- infra/prod/main.tf
+++ infra/prod/main.tf
@@ -11,26 +11,34 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# 1. Lookup the existing Zone (Manually created to avoid infinite hang)
+# Get the current Account ID dynamically
+data "aws_caller_identity" "current" {}
+
+# Define the ARN using the Account ID
+locals {
+  github_oidc_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+}
+
+# Lookup the existing Zone (Manually created to avoid infinite hang)
 data "aws_route53_zone" "main" {
   name = "justinklein.ca"
 }
 
-# 2. Lookup the existing Wildcard Certificate (*.justinklein.ca)
+# Lookup the existing Wildcard Certificate (*.justinklein.ca)
 data "aws_acm_certificate" "wildcard" {
   domain      = "justinklein.ca" 
   most_recent = true
   statuses    = ["ISSUED"]
 }
 
-# 3. Deploy the Sites
+# Deploy the Sites
 module "site_root" {
   source          = "../modules/website-static"
   site_domain     = "justinklein.ca"
   zone_id         = data.aws_route53_zone.main.zone_id
   acm_certificate_arn = data.aws_acm_certificate.wildcard.arn
   github_repo         = "justinklein2001/justinklein.ca" 
-  github_oidc_arn     = data.github_oidc_arn
+  github_oidc_arn     = local.github_oidc_arn
 }
 
 module "site_get_smart" {
@@ -39,5 +47,5 @@ module "site_get_smart" {
   zone_id         = data.aws_route53_zone.main.zone_id
   acm_certificate_arn = data.aws_acm_certificate.wildcard.arn
   github_repo         = "justinklein2001/my-tech-notes" 
-  github_oidc_arn     = data.github_oidc_arn
+  github_oidc_arn     = local.github_oidc_arn
 }
